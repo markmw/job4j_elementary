3c03141055dd496e13258ab87a90d736fb17ad2f
markmw <dant1kkz@gmail.com>
2021-10-24 12:33:18 +0600

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Adil Baitorynov");
+        System.out.println("02.06.1990");
+    }
+}
