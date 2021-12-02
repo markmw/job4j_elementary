a7309ec236a9e851a7c33806f6aa4d2853f44ce9
markmw <dant1kkz@gmail.com>
2021-12-02 20:11:41 +0600

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        }
+        if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = answer("Привет, бот.");
+        System.out.println(rsl);
+        rsl = answer("Пока.");
+        System.out.println(rsl);
+    }
+}
