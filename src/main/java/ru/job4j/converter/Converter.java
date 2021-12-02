80426f9d86f795b696578708c96a5a26ac679334
markmw <dant1kkz@gmail.com>
2021-12-02 18:39:41 +0600

    0. Что такое тестирование [#159431 #234703] - Исправление

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -21,6 +21,6 @@ public class Converter {
         expected = 2;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
-        System.out.println("140 rubles are 2. Test result: " + passed);
+        System.out.println("120 rubles are 2. Test result: " + passed);
     }
 }
