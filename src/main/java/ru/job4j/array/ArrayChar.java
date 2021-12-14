3d81f9493d62bf70e0c108edd71c4e3e4ea8863a
markmw <dant1kkz@gmail.com>
2021-12-14 18:44:12 +0600

    6.4. Слово начинается с ... [#41585 #239076] - Исправление

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        for (int i = 0; i <= pref.length - 1; i++) {
+        for (int i = 0; i < pref.length; i++) {
             if (word[i] != pref[i]) {
                 return false;
             }
